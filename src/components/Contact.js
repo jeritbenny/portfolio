20c5bec99d8e1304aa97fa300133ff1349f761e0
jeritbenny <jeritbenny.jbk@gmail.com>
2024-07-20 00:25:02 +0530

    emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import'./Contact.css'
+import emailjs from '@emailjs/browser'
 
 function Contact() {
+  const[name,setName]=useState('')
+  const[email,setEmail]=useState('')
+  const[message,setMessage]=useState('')
+
+  const handleSubmit= (e) =>{
+    e.preventDefault()
+
+    const serviceId="service_2mcn9az";
+    const templateId="template_7mcme1m";
+    const publicKey="8cKxuD2YNbyJ2_O-L";
+
+    const templateParams={
+      from_name: name,
+      from_email: email,
+      to_name:"jerit",
+      message: message
+    }
+    //sending msg
+    emailjs.send(serviceId,templateId,templateParams,publicKey)
+    .then((response)=>{
+      alert("email sended")
+      setMessage('');
+      setName('');
+      setEmail('');
+    })
+    .catch((error)=>{
+      console.log("error ",error);
+    })
+  }
   return (
     <div className='contact'>
       <h1 className='con-head'>Contact</h1>
@@ -19,20 +49,27 @@ function Contact() {
          </div>
       </div>
       <div className='contact-div1'>
+        <form onSubmit={handleSubmit}>
       <div className='input-group'>
-        <input type='text' required/>
+        <input type='text' required value={name}
+        onChange={(e)=>setName(e.target.value)}/>
         <label>Your Name</label>
       </div>
       <div className='input-group'>
-        <input type='email' required/>
+        <input type='email' required
+         value={email}
+         onChange={(e)=>setEmail(e.target.value)}/>
         <label>Your Email</label>
       </div>
       <div className='input-group'>
-        <textarea className='con-area' placeholder='Message'></textarea>
+        <textarea className='con-area' placeholder='Message'
+         value={message}
+         onChange={(e)=>setMessage(e.target.value)}></textarea>
       </div>
       <div>
         <button className='btn'>Sent<i class="fa-solid fa-paper-plane"></i></button>
       </div>
+      </form>
       </div>
       </div>
 
